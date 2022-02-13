f7ced6371ea68b87f171a0ecd2a38efd6ce1640d
İrem Doğancı <45331607+iremdgnc@users.noreply.github.com>
2022-02-13 22:19:44 +0300

    Update menu.py

--- app/models/menu.py
+++ app/models/menu.py
@@ -8,13 +8,13 @@ class Menu(db.Model):
     name = db.Column(db.String(255), unique=False, nullable=False)
     product = db.relationship('Product', backref='product')
 
-def insert_menu():
-    """default menu data"""
-    menu = Menu(rest_id="1",
-                name="Dombili Menu")
+    def insert_menu():
+        """default menu data"""
+        menu = Menu(rest_id="1",
+                    name="Dombili Menu")
 
-    menu2 = Menu(rest_id="2",
-                name="DubleMumle Menu")
-    db.session.add(menu)
-    db.session.add(menu2)
-    db.session.commit()
+        menu2 = Menu(rest_id="2",
+                    name="DubleMumle Menu")
+        db.session.add(menu)
+        db.session.add(menu2)
+        db.session.commit()
